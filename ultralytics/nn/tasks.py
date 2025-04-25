08cbcfdffd0f896f780a78c1479eb0b54b42990f
wissmz <mazouziwissam4@gmail.com>
2025-04-25 11:46:39 +0100

    ajoute C3K2 dans yolov8

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -68,6 +68,7 @@ from ultralytics.nn.modules import (
     YOLOEDetect,
     YOLOESegment,
     v10Detect,
+    C3K2_Custom,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
