c38d543b0e83bf4f32c570ad60ce366aaf479b1c
wissmz <mazouziwissam4@gmail.com>
2025-04-26 18:40:19 +0100

    ajoute swin block

--- ultralytics/nn/modules/ghostc2f.py
+++ ultralytics/nn/modules/ghostc2f.py
@@ -1,29 +1,24 @@
 import torch
 import torch.nn as nn
-
-from ultralytics.nn.modules import GhostConv  # attention à bien importer GhostConv
-from ultralytics.nn.modules import Concat
+from ultralytics.nn.modules import GhostConv, Concat
 
 class GhostC2f(nn.Module):
+    """GhostC2f module like C2f but using GhostConv"""
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
-        """
-        c1: nombre de channels en entrée
-        c2: nombre de channels en sortie
-        n: nombre de GhostConv à répéter
-        shortcut: utiliser ou non un residual
-        g: nombre de groupes pour Grouped Convolution
-        e: expansion ratio
-        """
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
-        self.cv1 = GhostConv(c1, c_, k=1, g=g)
-        self.cv2 = GhostConv(c1, c_, k=1, g=g)
-        self.cv3 = GhostConv(2 * c_, c2, k=1)
-        self.m = nn.Sequential(*[GhostConv(c_, c_, k=3, s=1, g=g) for _ in range(n)])
-        self.add = shortcut and c1 == c2
+        self.cv1 = GhostConv(c1, c_, 1, 1)
+        self.cv2 = GhostConv(c1, c_, 1, 1)
+        self.cv3 = GhostConv(2 * c_, c2, 1, 1)
+        self.m = nn.Sequential(*[GhostConv(c_, c_, 3, 1) for _ in range(n)])
+        self.shortcut = shortcut and c1 == c2
 
     def forward(self, x):
         y1 = self.cv1(x)
         y2 = self.cv2(x)
-        y = torch.cat((self.m(y1), y2), dim=1)
-        return self.cv3(y)
+        y1 = self.m(y1)
+        y = torch.cat((y1, y2), dim=1)
+        out = self.cv3(y)
+        if self.shortcut:
+            out = out + x
+        return out
