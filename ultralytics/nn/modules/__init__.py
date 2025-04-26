68fdd100774bf3deb77ce55f35cfeb8677d54a26
wissmz <mazouziwissam4@gmail.com>
2025-04-27 00:09:13 +0100

    ajoute swin block

--- ultralytics/nn/modules/__init__.py
+++ ultralytics/nn/modules/__init__.py
@@ -105,7 +105,7 @@ from .transformer import (
 
 from .swin_block import SwinBlock
 from .cbam import CBAM
-
+from .msda import MSDA
 __all__ = (
     "Conv",
     "Conv2",
@@ -185,7 +185,7 @@ __all__ = (
     "A2C2f",
     "SwinBlock",
     "CBAM",
-   
+    "MSDA",
 
 )
 
