323b96e568400524e4571e358d6fe8e9fbef1a53
wissmz <mazouziwissam4@gmail.com>
2025-04-27 10:46:20 +0100

    ajoute swin block

--- ultralytics/nn/modules/repc3k2.py
+++ ultralytics/nn/modules/repc3k2.py
@@ -1,28 +1,20 @@
 import torch
 import torch.nn as nn
-
-class RepConv2x2(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(RepConv2x2, self).__init__()
-        self.conv_2x2 = nn.Conv2d(in_channels, out_channels, 2, 1, 0, bias=False)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.act = nn.SiLU()
-
-    def forward(self, x):
-        return self.act(self.bn(self.conv_2x2(x)))
+import torch.nn.functional as F
 
 class RepC3K2(nn.Module):
-    def __init__(self, in_channels, out_channels, n=3):
+    def __init__(self, in_channels, out_channels):
         super(RepC3K2, self).__init__()
-        hidden_channels = int(out_channels * 0.5)
-
-        self.cv1 = RepConv2x2(in_channels, hidden_channels)
-        self.cv2 = RepConv2x2(in_channels, hidden_channels)
-        self.m = nn.Sequential(*[RepConv2x2(hidden_channels, hidden_channels) for _ in range(n)])
-        self.cv3 = RepConv2x2(2 * hidden_channels, out_channels)
+        # Define a 3x3 convolution followed by 2x2 convolution
+        self.conv_3x3 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv_2x2 = nn.Conv2d(out_channels, out_channels, kernel_size=2, stride=1, padding=0)
+        
+        # Batch Normalization and Activation
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.act = nn.ReLU()
 
     def forward(self, x):
-        y1 = self.cv1(x)
-        y2 = self.cv2(x)
-        y1 = self.m(y1)
-        return self.cv3(torch.cat((y1, y2), dim=1))
+        # Apply 3x3 conv -> 2x2 conv -> BatchNorm -> ReLU
+        x = self.act(self.bn(self.conv_3x3(x)))
+        x = self.act(self.bn(self.conv_2x2(x)))
+        return x
