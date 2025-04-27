24cb90654c5a1495f63a0149070058b454a7934f
wissmz <mazouziwissam4@gmail.com>
2025-04-27 19:24:10 +0100

    ajoutescbam parm  block

--- ultralytics/nn/modules/ecbam.py
+++ ultralytics/nn/modules/ecbam.py
@@ -2,18 +2,23 @@ import torch
 import torch.nn as nn
 
 class ECBAM(nn.Module):
-    def __init__(self, channels, reduction=16, kernel_size=7):
-        super(ECBAM, self).__init__()
+    """Efficient Channel and Spatial Attention Module"""
+    def __init__(self, c1, r=16):
+        super().__init__()
+        # Channel Attention
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
+        
+        # Make sure c1 is used as input and output channels
         self.fc = nn.Sequential(
-            nn.Conv2d(channels, channels // reduction, 1, bias=False),
-            nn.ReLU(),
-            nn.Conv2d(channels // reduction, channels, 1, bias=False)
+            nn.Conv2d(c1, c1 // r, kernel_size=1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(c1 // r, c1, kernel_size=1, bias=False)
         )
         self.sigmoid_channel = nn.Sigmoid()
         
-        self.conv_spatial = nn.Conv2d(2, 1, kernel_size, padding=kernel_size // 2, bias=False)
+        # Spatial Attention
+        self.conv = nn.Conv2d(2, 1, kernel_size=7, padding=3, bias=False)
         self.sigmoid_spatial = nn.Sigmoid()
 
     def forward(self, x):
@@ -22,11 +27,11 @@ class ECBAM(nn.Module):
         max_out = self.fc(self.max_pool(x))
         ca = self.sigmoid_channel(avg_out + max_out)
         x = x * ca
-
+        
         # Spatial Attention
         avg_out = torch.mean(x, dim=1, keepdim=True)
         max_out, _ = torch.max(x, dim=1, keepdim=True)
-        sa = self.sigmoid_spatial(self.conv_spatial(torch.cat([avg_out, max_out], dim=1)))
-        x = x * sa
-
-        return x
+        sa = torch.cat([avg_out, max_out], dim=1)
+        sa = self.sigmoid_spatial(self.conv(sa))
+        
+        return x * sa
