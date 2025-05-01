38b0a3b02edffe44a3685356d10063c5e06c4f14
wissmz <mazouziwissam4@gmail.com>
2025-05-01 13:49:48 +0100

    modifier c2fSE

--- ultralytics/nn/modules/c2f_SE.py
+++ ultralytics/nn/modules/c2f_SE.py
@@ -27,7 +27,8 @@ class C2f_SE(nn.Module):
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, 2 * c_, 1, 1)
         #self.cv2 = Conv((2 + n) * c_, c2, 1, 1)
-        self.cv2 = Conv(int((2 + n) * c_), c2, 1, 1)
+        #self.cv2 = Conv(int((2 + n) * c_), c2, 1, 1)
+        self.cv2 = Conv(int((2 + n) * c_), c2, (1, 1), (1, 1))
 
         self.m = nn.ModuleList([
             Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)
