cdc93a78b86755214e256130571946a1aaa42046
wissmz <mazouziwissam4@gmail.com>
2025-05-01 14:44:05 +0100

    coorg auto

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -25,16 +25,21 @@ __all__ = (
 )
 
 
-def autopad(k, p=None, d=1):  # kernel, padding, dilation
-    """Pad to 'same' shape outputs."""
+# def autopad(k, p=None, d=1):  # kernel, padding, dilation
+#     """Pad to 'same' shape outputs."""
+#     if d > 1:
+#         k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+#     if p is None:
+#         p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+#     print(f"autopad -> k: {k}, p: {p}")  # debug print
+#     return p
+
+def autopad(k, p=None, d=1):
     if d > 1:
-        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]
     if p is None:
-        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
-    print(f"autopad -> k: {k}, p: {p}")  # debug print
-    return p
-
-
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]
+    return p if isinstance(p, int) else tuple(p)  # ← fix ici
 class Conv(nn.Module):
     """
     Standard convolution module with batch normalization and activation.
