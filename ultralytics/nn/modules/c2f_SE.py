b45674d4daf5ca469dbf7cc4365481692f00ef31
wissmz <mazouziwissam4@gmail.com>
2025-05-01 14:09:27 +0100

    C2F_SE

--- ultralytics/nn/modules/c2f_SE.py
+++ ultralytics/nn/modules/c2f_SE.py
@@ -1,44 +1,35 @@
 import torch
 import torch.nn as nn
+from ultralytics.nn.modules.conv import Conv
 
-from ultralytics.nn.modules.conv import Conv  # Ultralytics custom Conv
-from ultralytics.nn.modules.block import Bottleneck  # Import Bottleneck
-
-class SEBlock(nn.Module):
-    """Squeeze-and-Excitation block."""
-    def __init__(self, c, reduction=16):
-        super().__init__()
-        self.pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Conv2d(c, c // reduction, 1, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(c // reduction, c, 1, bias=False),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        return x * self.fc(self.pool(x))
-
-
-class C2f_SE(nn.Module):
-    """C2f block with Squeeze-and-Excitation (SE) attention."""
+class C2F_SE(nn.Module):
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
         super().__init__()
-        c_ = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, 2 * c_, 1, 1)
-        #self.cv2 = Conv((2 + n) * c_, c2, 1, 1)
-        #self.cv2 = Conv(int((2 + n) * c_), c2, 1, 1)
-        self.cv2 = Conv(int((2 + n) * c_), c2, (1, 1), (1, 1))
-
+        c_ = int(c2 * e)  # intermediate channels
+        self.cv1 = Conv(c1, 2 * c_, 1, 1)  # pointwise convolution
+        self.cv2 = Conv((2 + n) * c_, c2, 1, 1)  # corrected to use int kernel/stride
         self.m = nn.ModuleList([
-            Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)
+            Conv(c_, c_, 3, 1, g=g) for _ in range(n)
         ])
-        self.se = SEBlock(c2)
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(c2, c2 // 16, 1, 1, 0),
+            nn.ReLU(),
+            nn.Conv2d(c2 // 16, c2, 1, 1, 0),
+            nn.Sigmoid()
+        )
+        self.shortcut = shortcut and c1 == c2
 
     def forward(self, x):
-        y1, y2 = self.cv1(x).chunk(2, 1)
-        outs = [y1, y2]
-        for i in self.m:
-            y2 = i(y2)
-            outs.append(y2)
-        return self.se(self.cv2(torch.cat(outs, 1)))
+        y = self.cv1(x)
+        y1, y2 = y.chunk(2, 1)
+        y_ = [y1]
+        for m in self.m:
+            y1 = m(y1)
+            y_.append(y1)
+        y = self.cv2(torch.cat(y_ + [y2], 1))
+        s = self.se(y)
+        y = y * s
+        if self.shortcut:
+            y = x + y
+        return y
