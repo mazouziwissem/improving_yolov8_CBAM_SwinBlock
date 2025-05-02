077ee8b202697f70e14664f92f627c0fe10ed05f
wissmz <mazouziwissam4@gmail.com>
2025-05-02 19:15:11 +0100

    C3K2

--- ultralytics/nn/modules/c3k2.py
+++ ultralytics/nn/modules/c3k2.py
@@ -1,26 +1,25 @@
 import torch
 import torch.nn as nn
-
-from ultralytics.nn.modules.conv import Conv
+from ultralytics.nn.modules import Conv
 
 class C3K2(nn.Module):
-    def __init__(self, c1, c2, n=1, shortcut=True, g=4):
+    def __init__(self, c1, c2, n=1, g=4):  # Signature simplifiée
         super().__init__()
-        self.g = g
+        self.c_ = (c2 // (2 * g)) * g  # Garantit la divisibilité
         
-        # Calcul sécurisé des canaux
-        self.c_ = max((c2 // (2 * g)) * g, g)  # Garantit c_ >= g et divisible par g
-        
-        self.cv1 = Conv(c1, self.c_, 1, 1)
-        self.cv2 = Conv(c1, self.c_, 1, 1)
+        self.cv1 = Conv(c1, self.c_, 1)
+        self.cv2 = Conv(c1, self.c_, 1)
         
         self.m = nn.Sequential(*[
             nn.Sequential(
-                Conv(self.c_, self.c_, 3, 1, g=self.g),
-                Conv(self.c_, self.c_, 1, 1)
+                Conv(self.c_, self.c_, 3, 1, g=g),
+                Conv(self.c_, self.c_, 1)
             ) for _ in range(n)])
         
         self.cv3 = Conv(2 * self.c_, c2, 1)
 
     def forward(self, x):
-        return self.cv3(torch.cat([self.m(self.cv1(x)), self.cv2(x)], 1))
+        return self.cv3(torch.cat([
+            self.m(self.cv1(x)),
+            self.cv2(x)
+        ], 1))
