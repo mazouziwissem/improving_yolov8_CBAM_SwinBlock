2819d5d1b923b1e96c69f523f04d5718515eef5c
wissmz <mazouziwissam4@gmail.com>
2025-05-03 16:07:06 +0100

    bottleneck trans new mprov

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -87,8 +87,8 @@ from ultralytics.nn.modules import (
     YOLOEDetect,
     YOLOESegment,
     v10Detect,
-    CBAM,
-    SwinBlock,
+    # CBAM,
+    # SwinBlock,
 
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
