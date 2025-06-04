dc3379cb255b3a2799b5fd724bfd3889556921f4
wissmz <mazouziwissam4@gmail.com>
2025-06-04 19:23:42 +0100

    update swinblock

--- ultralytics/nn/modules/swin_block.py
+++ ultralytics/nn/modules/swin_block.py
@@ -1,60 +1,161 @@
-# ultralytics/nn/modules/swin_block.py
+# # ultralytics/nn/modules/swin_block.py
+# import torch
+# import torch.nn as nn
+# from einops import rearrange
+# import torch.nn.functional as F
+
+
+# def window_partition(x, window_size):
+#     # x: [B, H, W, C]
+#     B, H, W, C = x.shape
+#     x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
+#     windows = x.permute(0, 1, 3, 2, 4, 5).reshape(-1, window_size * window_size, C)
+#     return windows  # shape: [num_windows*B, window_size*window_size, C]
+
+# def window_reverse(windows, window_size, H, W):
+#     # windows: [num_windows*B, window_size*window_size, C]
+#     B = int(windows.shape[0] / (H * W / window_size / window_size))
+#     x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
+#     x = x.permute(0, 1, 3, 2, 4, 5).reshape(B, H, W, -1)
+#     return x  # shape: [B, H, W, C]
+
+
+# class SwinBlock(nn.Module):
+#     def __init__(self, dim, num_heads=2, window_size=7):
+#         super().__init__()
+#         self.dim = dim
+#         self.window_size = window_size
+#         self.norm1 = nn.LayerNorm(dim)
+#         self.attn = nn.MultiheadAttention(embed_dim=dim, num_heads=num_heads, batch_first=True)
+#         self.norm2 = nn.LayerNorm(dim)
+#         self.mlp = nn.Sequential(
+#             nn.Linear(dim, dim * 4),
+#             nn.GELU(),
+#             nn.Linear(dim * 4, dim)
+#         )
+
+#     def forward(self, x):
+#         B, C, H, W = x.shape
+
+       
+#         pad_h = (self.window_size - H % self.window_size) % self.window_size
+#         pad_w = (self.window_size - W % self.window_size) % self.window_size
+#         x = F.pad(x, (0, pad_w, 0, pad_h))  # pad last two dims
+
+#         Hp, Wp = x.shape[2], x.shape[3]
+
+#         x = rearrange(x, 'b c h w -> b h w c')
+#         x_windows = window_partition(x, self.window_size)  # [num_windows*B, ws*ws, C]
+
+#         x_windows = self.norm1(x_windows)
+#         attn_windows, _ = self.attn(x_windows, x_windows, x_windows)  # [num_windows*B, ws*ws, C]
+#         x_windows = x_windows + attn_windows
+#         x_windows = x_windows + self.mlp(self.norm2(x_windows))
+
+#         x = window_reverse(x_windows, self.window_size, Hp, Wp)  # [B, H', W', C]
+#         x = rearrange(x, 'b h w c -> b c h w')
+
+#         return x[:, :, :H, :W]  # remove padding
+
+
 import torch
 import torch.nn as nn
-from einops import rearrange
 import torch.nn.functional as F
+from einops import rearrange
 
-
-def window_partition(x, window_size):
-    # x: [B, H, W, C]
-    B, H, W, C = x.shape
-    x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
-    windows = x.permute(0, 1, 3, 2, 4, 5).reshape(-1, window_size * window_size, C)
-    return windows  # shape: [num_windows*B, window_size*window_size, C]
-
-def window_reverse(windows, window_size, H, W):
-    # windows: [num_windows*B, window_size*window_size, C]
-    B = int(windows.shape[0] / (H * W / window_size / window_size))
-    x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
-    x = x.permute(0, 1, 3, 2, 4, 5).reshape(B, H, W, -1)
-    return x  # shape: [B, H, W, C]
-
-
-class SwinBlock(nn.Module):
-    def __init__(self, dim, num_heads=2, window_size=7):
+class WindowAttention(nn.Module):
+    def __init__(self, dim, window_size, num_heads):
         super().__init__()
         self.dim = dim
+        self.window_size = window_size  # Wh, Ww
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=True)
+        self.attn_drop = nn.Dropout(0.0)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(0.0)
+
+    def forward(self, x):
+        B_, N, C = x.shape
+        qkv = self.qkv(x).reshape(B_, N, 3, self.num_heads, C // self.num_heads)
+        qkv = qkv.permute(2, 0, 3, 1, 4)  # 3, B_, num_heads, N, head_dim
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B_, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+class SwinBlock(nn.Module):
+    def __init__(self, dim, input_resolution, num_heads, window_size=7, shift_size=0):
+        super().__init__()
+        self.dim = dim
+        self.input_resolution = input_resolution
+        self.num_heads = num_heads
         self.window_size = window_size
+        self.shift_size = shift_size
+
         self.norm1 = nn.LayerNorm(dim)
-        self.attn = nn.MultiheadAttention(embed_dim=dim, num_heads=num_heads, batch_first=True)
+        self.attn = WindowAttention(dim, window_size=window_size, num_heads=num_heads)
         self.norm2 = nn.LayerNorm(dim)
         self.mlp = nn.Sequential(
-            nn.Linear(dim, dim * 4),
+            nn.Linear(dim, 4 * dim),
             nn.GELU(),
-            nn.Linear(dim * 4, dim)
+            nn.Linear(4 * dim, dim),
         )
 
     def forward(self, x):
-        B, C, H, W = x.shape
+        H, W = self.input_resolution
+        B, L, C = x.shape
+        assert L == H * W, "Input feature has wrong size"
 
-       
-        pad_h = (self.window_size - H % self.window_size) % self.window_size
-        pad_w = (self.window_size - W % self.window_size) % self.window_size
-        x = F.pad(x, (0, pad_w, 0, pad_h))  # pad last two dims
+        shortcut = x
+        x = self.norm1(x)
+        x = x.view(B, H, W, C)
 
-        Hp, Wp = x.shape[2], x.shape[3]
+        # cyclic shift
+        if self.shift_size > 0:
+            shifted_x = torch.roll(x, shifts=(-self.shift_size, -self.shift_size), dims=(1, 2))
+        else:
+            shifted_x = x
 
-        x = rearrange(x, 'b c h w -> b h w c')
-        x_windows = window_partition(x, self.window_size)  # [num_windows*B, ws*ws, C]
+        # partition windows
+        x_windows = window_partition(shifted_x, self.window_size)  # nW*B, window_size, window_size, C
+        x_windows = x_windows.view(-1, self.window_size * self.window_size, C)
 
-        x_windows = self.norm1(x_windows)
-        attn_windows, _ = self.attn(x_windows, x_windows, x_windows)  # [num_windows*B, ws*ws, C]
-        x_windows = x_windows + attn_windows
-        x_windows = x_windows + self.mlp(self.norm2(x_windows))
+        # W-MSA/SW-MSA
+        attn_windows = self.attn(x_windows)
 
-        x = window_reverse(x_windows, self.window_size, Hp, Wp)  # [B, H', W', C]
-        x = rearrange(x, 'b h w c -> b c h w')
+        # merge windows
+        attn_windows = attn_windows.view(-1, self.window_size, self.window_size, C)
+        shifted_x = window_reverse(attn_windows, self.window_size, H, W)
 
-        return x[:, :, :H, :W]  # remove padding
+        # reverse cyclic shift
+        if self.shift_size > 0:
+            x = torch.roll(shifted_x, shifts=(self.shift_size, self.shift_size), dims=(1, 2))
+        else:
+            x = shifted_x
+        x = x.view(B, H * W, C)
 
+        # FFN
+        x = shortcut + x
+        x = x + self.mlp(self.norm2(x))
+        return x
 
+def window_partition(x, window_size):
+    B, H, W, C = x.shape
+    x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
+    windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
+    return windows
+
+def window_reverse(windows, window_size, H, W):
+    B = int(windows.shape[0] / (H * W / window_size / window_size))
+    x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
+    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
+    return x
